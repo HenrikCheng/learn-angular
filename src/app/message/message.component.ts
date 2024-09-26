0e3e0c65e40b68193d99f776e078032f10e45960
HenrikCheng <henrikcheng@live.se>
2024-09-26 13:15:57 +0200

    fix: remove unneeded css files

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -4,7 +4,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css'],
 })
 export class MessageComponent {
   @Input() message: string = ''; // Like props in React
