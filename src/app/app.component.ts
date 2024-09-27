3ade39e4f8b7404d93d90c3e3982ddb2e90cd552
HenrikCheng <henrikcheng@live.se>
2024-09-27 11:02:59 +0200

    feat: wip console log window and el width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Event title';
-  selectedTitle: string | null = null; // To store the selected title
+  selectedTitle: string | null = null;
+  width: number | null = null;
+  windowWidth: number | null = null;
+
+  @ViewChild('eventTitle', { static: false }) eventTitle!: ElementRef;
+
+  getWidth() {
+    this.width = this.eventTitle.nativeElement.getBoundingClientRect().width;
+    this.windowWidth = window.innerWidth;
+    console.log('Width of h1 element:', this.width);
+    console.log('Width window:', window.innerWidth);
+  }
+
+  ngAfterViewInit() {
+    this.getWidth();
+  }
 
   onTitleSelected(title: string) {
-    this.selectedTitle = title; // Store the received title
-    console.log('Received title:', this.selectedTitle); // For debugging
+    this.selectedTitle = title;
+    this.getWidth();
   }
 }
