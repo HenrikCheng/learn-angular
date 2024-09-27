75fdf3135a0540e0976b918b6e2cc62d01abff72
HenrikCheng <henrikcheng@live.se>
2024-09-27 11:41:53 +0200

    feat: wip log if elem is greather than screenwidth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,21 @@ export class AppComponent implements AfterViewInit {
   selectedTitle: string | null = null;
   width: number | null = null;
   windowWidth: number | null = null;
+  TailwindTextSizes = [
+    'text-xs',
+    'text-sm',
+    'text-base',
+    'text-lg',
+    'text-xl',
+    'text-2xl',
+    'text-3xl',
+    'text-4xl',
+    'text-5xl',
+    'text-6xl',
+    'text-7xl',
+    'text-8xl',
+    'text-9xl',
+  ] as const;
 
   @ViewChild('eventTitle', { static: false }) eventTitle!: ElementRef;
 
@@ -18,6 +33,17 @@ export class AppComponent implements AfterViewInit {
     this.windowWidth = window.innerWidth;
     console.log('Width of h1 element:', this.width);
     console.log('Width window:', window.innerWidth);
+    this.compareWidth();
+  }
+
+  compareWidth() {
+    if (this.windowWidth && this.width && this.windowWidth < this.width) {
+      console.log('Window width is less than the element width');
+      // Increment size
+    } else {
+      console.log('Window width is greater than or equal to the element width');
+      // Check if below size is greater than window, if not, decrement size
+    }
   }
 
   ngAfterViewInit() {
