419f15c65e84e2f4223c25d8731ebcd377866859
HenrikCheng <henrikcheng@live.se>
2024-09-27 13:31:23 +0200

    refactor: remove unneeded css file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements AfterViewInit {
   title = 'Event title';
